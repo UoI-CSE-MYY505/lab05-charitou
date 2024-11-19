689d7f69400c831db02c8a33ad14ae580545faa5
charitou <cs05556@uoi.gr>
2024-11-19 20:28:32 +0200

    Completed lab05

--- lab05.s
+++ lab05.s
@@ -65,8 +65,9 @@ next:
 taken:
 
 # ----------------------------------------------------------------------------------------
-# TODO: Add an example where an instruction passes its result to the 2nd following instruction
-# There should be no stalls
+    add t1,s0,s1  #t1 = 1
+    add t2,s0,s2  #t2 = 2
+    add t3,t1,s3  #t3 = 4
 # ----------------------------------------------------------------------------------------
     # nop instructions added between examples
     add  zero, zero, zero  
@@ -74,10 +75,9 @@ taken:
     add  zero, zero, zero  
 
 # ----------------------------------------------------------------------------------------
-# TODO: Add an example with a double hazard and check that it works corretly.
-# A double hazzard is when the source register of an instruction matches the destination
-#  registers of both of the two instructions preceeding it. It should get the newest value.
-# There should be no stalls
+    add t1,s0,s1  #t1 = 1
+    add t1,s0,s2  #t1 = 2
+    add t3,t1,s3  #t3 = 5
 # ----------------------------------------------------------------------------------------
     # nop instructions added between examples
     add  zero, zero, zero  
@@ -85,8 +85,8 @@ taken:
     add  zero, zero, zero  
 
 # ----------------------------------------------------------------------------------------
-# TODO: Add an example with a load stalling for 1 cycle to pass a value to a NOT-TAKEN branch 
-#  Is this a data hazard or a control hazard?
+    lw t3,4(a0)
+    beq t3,zero,exit
 # ----------------------------------------------------------------------------------------
     # nop instructions added between examples
     add  zero, zero, zero  
@@ -94,7 +94,10 @@ taken:
     add  zero, zero, zero  
 
 # ----------------------------------------------------------------------------------------
-# TODO: Add an example with taken branch to a label which is immediately following the branch
+    beq zero,s0,nextInstr
+nextInstr:
+    add t0,s1,s2
+    add t1,s2,s3
 # ----------------------------------------------------------------------------------------
 
 
